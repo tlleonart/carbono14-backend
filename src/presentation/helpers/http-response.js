2f658f72d92fd149d124c6bc29e2769d98fa5fef
tomaslleonart <tomas@itbaf.com>
2023-07-26 22:36:41 -0300

    moved errors to its own folder

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,5 +1,5 @@
-const ServerError = require('./server-error')
-const UnauthorizedError = require('./unauthorized-error')
+const ServerError = require('../errors/server-error')
+const UnauthorizedError = require('../errors/unauthorized-error')
 
 module.exports = class HttpResponse {
   static badRequest (error) {
