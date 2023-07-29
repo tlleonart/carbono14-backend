0a55d1c24f89cdf1cd3fff36fcd999e18e768647
tomaslleonart <tomas@itbaf.com>
2023-07-29 17:56:18 -0300

    ensure AuthUseCase throws if no dependency is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -87,6 +87,12 @@ describe('Auth UseCase', () => {
     expect(loadUserByEmailRepositorySpy.email).toBe('any@email.com')
   })
 
+  test('Should throw if dependency is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any@email.com', 'any_password')
+    expect(promise).rejects.toThrow()
+  })
+
   test('Should throw if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any@email.com', 'any_password')
