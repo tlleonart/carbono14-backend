705cec8075d5f1385eea501eb57767714e2d2de5
tomaslleonart <tomas@itbaf.com>
2023-07-30 15:09:45 -0300

    mapped main routes

---
+++ src/main/config/routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const fb = require('fast-glob')
+
+module.exports = app => {
+  app.use('/api', router)
+
+  // Reads all files inside routes folder, then dynamically imports routes.
+
+  fb.sync('**/src/main/routes/**.js').forEach(file => {
+    require(`../../../${file}`)(router)
+  })
+}
