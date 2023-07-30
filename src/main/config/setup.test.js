b5771aea2b95d0321c33cb796fc9ee457a21a7a2
tomaslleonart <tomas@itbaf.com>
2023-07-29 22:26:53 -0300

    enable CORS

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe('App Setup', () => {
+  test('Should disable x-powered-by header', async () => {
+    app.get('/test_x_powered_by', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_x_powered_by')
+
+    expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
+})
--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,4 @@
-const express = require('express')
-const app = express()
+const app = require('./config/app')
 const PORT = 5858
 
 app.listen(PORT, () => console.info(`Server running on port: ${PORT}`))
